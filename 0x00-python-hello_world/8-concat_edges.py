60c2c31e30798a33fb2314c0a38afe8312a5bea1
Adunolami <abinatitilayo2@gmail.com>
2023-10-02 19:12:20 -0700

    update

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interative, object-oriented programming\
+        language that comnines remakable power with very
+        clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
