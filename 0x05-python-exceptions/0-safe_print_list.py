a9305355482609d07ead701345ddea21609270ac
Adunolami <abinatitilayo2@gmail.com>
2023-10-23 18:24:09 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,9 @@ def safe_print_list(my_list=[], x=0):
     ret = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end = "")
+            print("{}".format(my_list[i]), end="")
             rct += 1
         except IndexError:
             break
-        print("")
-        return(ret)
+    print("")
+    return(ret)
