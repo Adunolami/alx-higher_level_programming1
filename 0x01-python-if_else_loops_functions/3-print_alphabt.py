ed3f07a789a5eb10450dab4d8e46e765c7714f72
Adunolami <abinatitilayo2@gmail.com>
2023-11-07 13:49:12 -0800

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-# 2-print_alphabet.py
-"""Print the alphabet in lowercase, not followed by a new line."""
-for char in range(97, 123):
-    if chr(char) not in ['q', 'e']:
-        print(chr(char), end='')
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
